6c1caa3434a8e81679a41fcd2d7aea1fad93cc10
arik zaki <yusufshadiq31@gmail.com>
2024-04-03 22:53:23 +0700

    add: meta tags for bots

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,21 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Shadiq's homepage" />
         <meta name="author" content="Yusuf Shadiq" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="Yusuf Shadiq" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Yusuf Shadiq" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:site" content="shadiq" />
+        <meta name="twitter:creator" content="shadiq" />
+        <meta name="twitter:image" content="https://www.shadiq.id/blob_card.png" />
         <meta property="og:site_name" content="Yusuf Shadiq" />
         <meta name="og:title" content="Yusuf Shadiq" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://www.shadiq.id/blob_card.png" />
+        <meta name="google-site-verification" content="qvnKPyOMUOkZRlO87tcdjY2BzxyTLT-wqFb2PBKp9Fs" />
         <title>Yusuf Shadiq - Homepage</title>
       </Head>
 
