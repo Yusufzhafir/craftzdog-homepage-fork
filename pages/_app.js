6c4d1f867a82350eaa7365169b03b7e3fc9a7c6c
arik zaki <yusufshadiq31@gmail.com>
2024-04-03 15:40:43 +0700

    add: tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,16 @@ import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import { Analytics } from '@vercel/analytics/react'
 import { Plus_Jakarta_Sans } from 'next/font/google'
-// import Payhip from '../components/payhip'
-// import Fonts from '../components/fonts'
+import '../public/style.css'
 
 const JKTSans = Plus_Jakarta_Sans({
   display: 'swap',
   subsets: ['latin'],
   weight: ['700', '500']
 })
-if (typeof window !== 'undefined') {
-  window.history.scrollRestoration = 'manual'
-}
+// if (typeof window !== 'undefined') {
+//   window.history.scrollRestoration = 'manual'
+// }
 
 function Website({ Component, pageProps, router }) {
   return (
